6a0f6eaae11e83e62657cf1d3235ec913abd44d5
thaiiss7 <thaismichel7@gmail.com>
2025-07-26 17:33:44 -0300

    started like endpoints

--- Endpoints/LikeEndpoints.cs
+++ Endpoints/LikeEndpoints.cs
@@ -4,6 +4,25 @@ public static class LikeEndpoints
 {
     public static void ConfigureLikeEndpoints(this WebApplication app)
     {
-        
+        app.MapPost("like/add", async (
+            [FromBody] LikeCreatePayload like,
+            [FromServices] ILikeRepository repo) =>
+            {
+                // var profiles = await repo.GetProfilesByIds(like.ProfilesIds);
+                // if (profiles.Count == 0)
+                //     return Results.NotFound("No profiles found with the provided IDs");
+
+                // foreach (var profile in profiles)
+                // {
+                //     await repo.Create(new Like
+                //     {
+                //         ProfileId = profile.Id,
+                //         LikedAt = DateTime.UtcNow
+                //     });
+                // }
+
+                // return Results.Ok();
+            }
+        );
     }
 }
